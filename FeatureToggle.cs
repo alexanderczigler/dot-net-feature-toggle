1ef7dff61f3cb69c432c0147fb42ecce59653441
Alexander Czigler <ilix@users.noreply.github.com>
2016-01-27 19:29:11 +0100

    Create FeatureToggle.cs

---
+++ FeatureToggle.cs
@@ -0,0 +1,37 @@
+using System.Collections.Generic;
+
+namespace Ilix.Configuration
+{
+    public class FeatureToggle : IFeatureToggle
+    {
+        public bool Enabled(string feature)
+        {
+            if (!FeatureToggles().ContainsKey(feature))
+                return false;
+
+            return FeatureToggles()[feature];
+        }
+
+        public bool Disabled(string feature)
+        {
+            return !Enabled(feature);
+        }
+
+        private Dictionary<string, bool> FeatureToggles()
+        {
+            var result = new Dictionary<string, bool>();
+            var featureToggles = (System.Collections.Hashtable)System.Configuration.ConfigurationManager.GetSection("FeatureToggles");
+            if (featureToggles != null)
+            {
+                System.Collections.ICollection featureToggleFeatures =
+                    ((System.Collections.Hashtable)
+                        System.Configuration.ConfigurationManager.GetSection("FeatureToggles")).Keys;
+                foreach (string feature in featureToggleFeatures)
+                {
+                    result.Add(feature, featureToggles[feature].ToString() == "On");
+                }
+            }
+            return result;
+        }
+    }
+}
